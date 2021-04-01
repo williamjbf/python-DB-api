aa5106f93f8efb55419d0f0091b0c958ba4cfb08
William <williamjbf@gmail.com>
2021-04-01 19:53:26 -0300

    exemplo simples de cursor

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 import MySQLdb
 
 MySQLdb.apilevel = '1.0'
-db = MySQLdb.connect(user="usuario",passwd="senha",db="nome_DB",host="localhost",port=3306,autocommit=True)
+db = MySQLdb.connect(user="usuario", passwd="senha", db="nome_DB", host="localhost", port=3306,
+                     autocommit=True)
 print("conectado")
+
+cursor = db.cursor()
+cursor.execute("SELECT * FROM cliente")
+print(cursor.fetchall())   # todos as consultas
+print(cursor.fetchone())   # apenas a primeira consulta
+print(cursor.fetchmany())  # passar a quantidade de consultas
 db.close()
