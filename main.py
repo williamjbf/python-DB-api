13c8456f44dd9fed1c9bf4b1f54bc32de46a5c79
William <williamjbf@gmail.com>
2021-04-01 19:06:21 -0300

    conexao DB generia

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import MySQLdb
 
 MySQLdb.apilevel = '1.0'
-db = MySQLdb.connect(user="usuario",passwd="usuario",db="treinaweb_clientes",host="localhost",port=3306,autocommit=True)
+db = MySQLdb.connect(user="usuario",passwd="senha",db="nome_DB",host="localhost",port=3306,autocommit=True)
 print("conectado")
 db.close()
