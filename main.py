13678aae741487346576e26cb9a3621609a42be0
William <williamjbf@gmail.com>
2021-04-02 16:07:48 -0300

    transacoes joins update e delete

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
 import MySQLdb
 
 MySQLdb.apilevel = '1.0'
-db = MySQLdb.connect(user="usuario", passwd="senha", db="nome_DB", host="localhost", port=3306,
-                     autocommit=True)
+db = MySQLdb.connect(user="root", passwd="will2410", db="treinaweb_clientes", host="localhost", port=3306,
+                     autocommit=False)
 print("conectado")
 
 cursor = db.cursor()
+try:
+    db.begin()
+    cursor.execute("INSERT INTO cliente (nome, idade) VALUES('José',25)")
+    cursor.execute("INSERT INTO cliente (nome, idade) VALUES('Maria',25)")
+    db.commit()
+except:
+    db.rollback()
+
 cursor.execute("SELECT * FROM cliente")
-print(cursor.fetchall())   # todos as consultas
-print(cursor.fetchone())   # apenas a primeira consulta
-print(cursor.fetchmany())  # passar a quantidade de consultas
+print(cursor.fetchall())
+print(cursor.lastrowid)
+
+cursor.execute("UPDATE cliente SET nome='Ana' WHERE idcliente=2")
+cursor.execute("SELECT * FROM cliente")
+print(cursor.fetchmany(50))
+
+cursor.execute("DELETE FROM cliente WHERE idcliente=2")
+cursor.execute("SELECT * FROM cliente")
+print(cursor.fetchall())
 db.close()
